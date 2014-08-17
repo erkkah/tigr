1f29e330dfaede6652ee7f5e7dab1782cf2b15dc
Richard Mitton <richard@codersnotes.com>
2014-08-17 16:44:02 -0700

    Cleaned up examples.

---
+++ examples/hello/hello.c
@@ -0,0 +1,14 @@
+#include "tigr.h"
+
+int main(int argc, char *argv[])
+{
+	Tigr *screen = tigrWindow(320, 240, "Hello", 0);
+	while (!tigrClosed(screen))
+	{
+		tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+		tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff), "Hello, world.");
+		tigrUpdate(screen);
+	}
+	tigrFree(screen);
+	return 0;
+}
