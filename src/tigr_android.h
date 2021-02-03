80be502211ab618e8436a983595eab04e10a3b42
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-03 21:00:14 +0100

    New take

---
+++ src/tigr_android.h
@@ -0,0 +1,32 @@
+#ifndef TIGR_ANDROID_H
+#define TIGR_ANDROID_H
+#ifdef __ANDROID__
+
+#include <android/input.h>
+#include <android/native_window.h>
+
+typedef enum {
+    AE_INPUT,
+    AE_WINDOW_CREATED,
+    AE_WINDOW_DESTROYED,
+    AE_ACTIVITY_DESTROYED,
+} AndroidEventType;
+
+typedef struct {
+    AndroidEventType type;
+    AInputEvent* inputEvent;
+    ANativeWindow* window;
+} AndroidEvent;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int android_pollEvent(int (*eventHandler)(AndroidEvent, void*), void*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __ANDROID__
+#endif  // TIGR_ANDROID_H
