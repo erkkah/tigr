01df2e5115b74df1efdc05000b267b5667842d38
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-04 20:17:38 +0100

    Load file from asset

--- tigr.c
+++ tigr.c
@@ -849,96 +849,124 @@ int tigrSaveImage(const char *fileName, Tigr *bmp)
 #include <stdio.h>
 #include <stdlib.h>
 
-void *tigrReadFile(const char *fileName, int *length)
-{
-	// TODO - unicode?
-	FILE *file;
-	char *data;
-	size_t len;
+#ifndef __ANDROID__
 
-	if (length)
-		*length = 0;
+void* tigrReadFile(const char* fileName, int* length) {
+    // TODO - unicode?
+    FILE* file;
+    char* data;
+    size_t len;
 
-	file = fopen(fileName, "rb");
-	if (!file)
-		return NULL;
+    if (length)
+        *length = 0;
 
-	fseek(file, 0, SEEK_END);
-	len = ftell(file);
-	fseek(file, 0, SEEK_SET);
+    file = fopen(fileName, "rb");
+    if (!file)
+        return NULL;
 
-	data = (char *)malloc(len+1);
-	if (!data)
-	{
-		fclose(file);
-		return NULL;
-	}
+    fseek(file, 0, SEEK_END);
+    len = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
-	if (fread(data, 1, len, file) != len) {
-		free(data);
-		fclose(file);
-		return NULL;
-	}
-	data[len] = '\0';
-	fclose(file);
+    data = (char*)malloc(len + 1);
+    if (!data) {
+        fclose(file);
+        return NULL;
+    }
 
-	if (length)
-		*length = len;
+    if (fread(data, 1, len, file) != len) {
+        free(data);
+        fclose(file);
+        return NULL;
+    }
+    data[len] = '\0';
+    fclose(file);
 
-	return data;
+    if (length)
+        *length = len;
+
+    return data;
 }
+
+#endif  // __ANDROID__
 
 // Reads a single UTF8 codepoint.
-const char *tigrDecodeUTF8(const char *text, int *cp)
-{
-	unsigned char c = *text++;
-	int extra = 0, min = 0;
-	*cp = 0;
-		 if (c >= 0xf0) { *cp = c & 0x07; extra = 3; min = 0x10000; }
-	else if (c >= 0xe0) { *cp = c & 0x0f; extra = 2; min = 0x800; }
-	else if (c >= 0xc0) { *cp = c & 0x1f; extra = 1; min = 0x80; }
-	else if (c >= 0x80) { *cp = 0xfffd; }
-	else *cp = c;
-	while (extra--) {
-		c = *text++;
-		if ((c & 0xc0) != 0x80) { *cp = 0xfffd; break; }
-		(*cp) = ((*cp) << 6) | (c & 0x3f);
-	}
-	if (*cp < min) *cp = 0xfffd;
-	return text;
+const char* tigrDecodeUTF8(const char* text, int* cp) {
+    unsigned char c = *text++;
+    int extra = 0, min = 0;
+    *cp = 0;
+    if (c >= 0xf0) {
+        *cp = c & 0x07;
+        extra = 3;
+        min = 0x10000;
+    } else if (c >= 0xe0) {
+        *cp = c & 0x0f;
+        extra = 2;
+        min = 0x800;
+    } else if (c >= 0xc0) {
+        *cp = c & 0x1f;
+        extra = 1;
+        min = 0x80;
+    } else if (c >= 0x80) {
+        *cp = 0xfffd;
+    } else {
+        *cp = c;
+    }
+    while (extra--) {
+        c = *text++;
+        if ((c & 0xc0) != 0x80) {
+            *cp = 0xfffd;
+            break;
+        }
+        (*cp) = ((*cp) << 6) | (c & 0x3f);
+    }
+    if (*cp < min) {
+        *cp = 0xfffd;
+    }
+    return text;
 }
 
-char *tigrEncodeUTF8(char *text, int cp)
-{
-	if (cp < 0 || cp > 0x10ffff) cp = 0xfffd;
+char* tigrEncodeUTF8(char* text, int cp) {
+    if (cp < 0 || cp > 0x10ffff) {
+        cp = 0xfffd;
+    }
 
-#define EMIT(X,Y,Z) *text++ = X | ((cp >> Y)&Z)
-		 if (cp <     0x80) { EMIT(0x00,0,0x7f); }
-	else if (cp <    0x800) { EMIT(0xc0,6,0x1f); EMIT(0x80, 0, 0x3f); }
-	else if (cp <  0x10000) { EMIT(0xe0,12,0xf); EMIT(0x80, 6, 0x3f); EMIT(0x80, 0, 0x3f); }
-	else                    { EMIT(0xf0,18,0x7); EMIT(0x80, 12, 0x3f); EMIT(0x80, 6, 0x3f); EMIT(0x80, 0, 0x3f); }
-	return text;
+#define EMIT(X, Y, Z) *text++ = X | ((cp >> Y) & Z)
+    if (cp < 0x80) {
+        EMIT(0x00, 0, 0x7f);
+    } else if (cp < 0x800) {
+        EMIT(0xc0, 6, 0x1f);
+        EMIT(0x80, 0, 0x3f);
+    } else if (cp < 0x10000) {
+        EMIT(0xe0, 12, 0xf);
+        EMIT(0x80, 6, 0x3f);
+        EMIT(0x80, 0, 0x3f);
+    } else {
+        EMIT(0xf0, 18, 0x7);
+        EMIT(0x80, 12, 0x3f);
+        EMIT(0x80, 6, 0x3f);
+        EMIT(0x80, 0, 0x3f);
+    }
+    return text;
 #undef EMIT
 }
 
-int tigrBeginOpenGL(Tigr *bmp)
-{
-   #ifdef TIGR_GAPI_GL
-   TigrInternal *win = tigrInternal(bmp);
-   win->gl.gl_user_opengl_rendering = 1;
-   return tigrGAPIBegin(bmp) == 0;
-   #else
-   return 0;
-   #endif
+int tigrBeginOpenGL(Tigr* bmp) {
+#ifdef TIGR_GAPI_GL
+    TigrInternal* win = tigrInternal(bmp);
+    win->gl.gl_user_opengl_rendering = 1;
+    return tigrGAPIBegin(bmp) == 0;
+#else
+    return 0;
+#endif
 }
 
-void tigrSetPostFX(Tigr *bmp, int hblur, int vblur, float scanlines, float contrast)
-{
-	TigrInternal *win = tigrInternal(bmp);
-	win->hblur = hblur;
-	win->vblur = vblur;
-	win->scanlines = scanlines;
-	win->contrast = contrast;
+void tigrSetPostFX(Tigr* bmp, int hblur, int vblur, float scanlines, float contrast) {
+    TigrInternal* win = tigrInternal(bmp);
+    win->hblur = hblur;
+    win->vblur = vblur;
+    win->scanlines = scanlines;
+    win->contrast = contrast;
 }
 
 //////// End of inlined file: tigr_utils.c ////////
@@ -3348,6 +3376,7 @@ extern "C" {
 
 int android_pollEvent(int (*eventHandler)(AndroidEvent, void*), void*);
 void android_swap(EGLDisplay display, EGLSurface surface);
+void* android_loadAsset(const char* filename, int* outLength);
 
 #ifdef __cplusplus
 }
@@ -3898,6 +3927,15 @@ static EGLint screenW = 0;
 static EGLint screenH = 0;
 static EGLConfig config = 0;
 static const EGLint contextAttribs[] = { EGL_CONTEXT_MAJOR_VERSION, 3, EGL_CONTEXT_MINOR_VERSION, 0, EGL_NONE };
+
+void* tigrReadFile(const char* fileName, int* length) {
+    if (length != 0) {
+        *length = 0;
+    }
+
+    void* asset = android_loadAsset(fileName, length);
+    return asset;
+}
 
 static EGLConfig getGLConfig(EGLDisplay display) {
     EGLConfig config = 0;
